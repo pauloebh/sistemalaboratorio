5a1a0187ec6737b594e564d7e81afcd406cf0a17
ccacsurf <ccacsurf@gmail.com>
2014-12-11 22:44:56 -0200

    para o log, ainda nao esta em uso

---
+++ src/main/java/com/aop/log/LogEventos.java
@@ -0,0 +1,91 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aop.log;
+
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.faces.context.FacesContext;
+
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.jsf.FacesContextUtils;
+
+import com.managed.bean.SessaoDoUsuario;
+import com.model.Log;
+import com.model.Usuario;
+import com.service.impl.LogService;
+
+//@Component
+//@Aspect
+public class LogEventos {
+
+    @Autowired
+    private LogService logService;
+
+
+    /* (non-Javadoc)
+	 * @see com.aop.log.ILogEventos#logAlvo(org.aspectj.lang.ProceedingJoinPoint)
+	 */
+    //@Around("execution(* com.service.*.*(..)) && @annotation(com.aop.log.Log)")
+    public Object logAlvo(/*ProceedingJoinPoint*/Object pjp) throws Throwable {
+        return logarAcao(pjp);
+    }
+
+    /* (non-Javadoc)
+	 * @see com.aop.log.ILogEventos#logarAcao(org.aspectj.lang.ProceedingJoinPoint)
+	 */
+    public Object logarAcao(/*ProceedingJoinPoint*/Object pjp) throws Throwable {
+
+    	//Object ret;
+        Object ret = pjp;
+
+        Log log = new Log();
+
+        log.setUsuario(this.getUsuarioLogado());
+        log.setData(new Date());
+
+        try {
+            //ret = pjp.proceed();
+            //logando acao realizada
+
+            //log.setDescricao(pjp.getSignature().getDeclaringType() +" ||| "+ pjp.getSignature().getName() +" ||| "+ pjp.getArgs());
+            logService.add(log);
+            
+        } catch (Throwable ex) {
+            Logger.getLogger(LogEventos.class.getName()).log(Level.SEVERE, null, ex);
+            
+            //log.setDescricao(pjp.getSignature().getDeclaringType() +" ||| "+ pjp.getSignature().getName() +" ||| "+ pjp.getArgs()+" ||| "+ ex.getMessage());            
+            logService.add(log);
+            
+            throw ex;
+        }
+        return ret;
+    }
+
+    public LogService getLogService() {
+        return logService;
+    }
+
+    public void setLogService(LogService logService) {
+        this.logService = logService;
+    }
+
+    /* (non-Javadoc)
+	 * @see com.aop.log.ILogEventos#getUsuarioLogado()
+	 */
+    public Usuario getUsuarioLogado() {
+
+        SessaoDoUsuario sessao = (SessaoDoUsuario) FacesContextUtils.getRequiredWebApplicationContext(
+                FacesContext.getCurrentInstance()).getBean("SessaoDoUsuario");
+
+        return sessao.getUsuarioLogado();
+
+    }
+}
