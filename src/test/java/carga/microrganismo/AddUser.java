2f7dccf770df11d73558f2e12bad497ae1a72bf9
ccacsurf <ccacsurf@gmail.com>
2014-12-14 19:30:41 -0200

    testNG funcionando... fazer download do plugin!

--- src/test/java/carga/microrganismo/AddUser.java
+++ src/test/java/carga/microrganismo/AddUser.java
@@ -1,4 +1,4 @@
-package com.dao.impl;
+package carga.microrganismo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
